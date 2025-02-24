31b26deac00cb2fa3a5f51d5de2f6ad85cef8787
Raheeq Ibrahim <113067381+raheeqi@users.noreply.github.com>
2025-02-24 17:07:20 -0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { get } from 'http';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
