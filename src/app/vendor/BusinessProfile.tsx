97fc1aca1bb47e3125e58653683223f9d70731af
Hoang Anh Vu <anhvu@bu.edu>
2025-02-24 23:27:27 -0500

    finish business profile frontend

--- src/app/vendor/BusinessProfile.tsx
+++ src/app/vendor/BusinessProfile.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../lib/firebase";
 import { useRouter } from 'next/router';
+import React, { useEffect, useState } from "react";
+import { db } from "../../lib/firebase";
 import Header from '../header';
 import '../tailwind.css';
 
@@ -17,7 +17,47 @@ const BusinessProfile = () => {
   const [loading, setLoading] = useState(true);
   const [businessName, setBusinessName] = useState("");
   const [legalBusinessName, setLegalBusinessName] = useState("");
+  const [contactLegalName, setContactLegalName] = useState("");
+  const [contactPreferredName, setContactPreferredName] = useState("");
+  const [country, setCountryName] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [aptSuite, setAptSuite] = useState("");
+  const [city, setCity] = useState("");
+  const [stateProvince, setStateProvince] = useState("");
+  const [zipPostalCode, setZipPostalCode] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [website, setWebsite] = useState("");
+  const [numberOfEmployees, setNumberOfEmployees] = useState("");
+  const [description, setDescription] = useState("");
+  const [facebookLink, setFacebookLink] = useState("");
+  const [twitterHandle, setTwitterHandle] = useState("");
+  const [instagramHandle, setInstagramHandle] = useState("");
+
   const router = useRouter();
+
+  const countries = [
+    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", 
+    "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", 
+    "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", 
+    "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic of the", 
+    "Congo, Republic of the", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", 
+    "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", 
+    "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", 
+    "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", 
+    "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", 
+    "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", 
+    "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", 
+    "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", 
+    "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", 
+    "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", 
+    "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", 
+    "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", 
+    "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", 
+    "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", 
+    "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", 
+    "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+  ];
 
   useEffect(() => {
     const fetchProfiles = async () => {
@@ -38,9 +78,75 @@ const BusinessProfile = () => {
     setLegalBusinessName(e.target.value);
   };
 
+  const handleContactLegalNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContactLegalName(e.target.value);
+  };
+
+  const handleContactPreferredNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContactPreferredName(e.target.value);
+  };
+
+  const handleCountryNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCountryName(e.target.value);
+  };
+
+  const handleStreetAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStreetAddress(e.target.value);
+  };
+
+  const handleAptSuiteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAptSuite(e.target.value);
+  };
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+  };
+
+  const handleStateProvinceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStateProvince(e.target.value);
+  };
+
+  const handleZipPostalCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setZipPostalCode(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+  };
+
+  const handleWebsiteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWebsite(e.target.value);
+  };
+
+  const handleNumberOfEmployeesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberOfEmployees(e.target.value);
+  };
+
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  };
+
+  const handleFacebookLinkChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFacebookLink(e.target.value);
+  };
+
+  const handleTwitterHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTwitterHandle(e.target.value);
+  };
+
+  const handleInstagramHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInstagramHandle(e.target.value);
+  };
+
   const handleNextStepClick = () => {
-    if (businessName) {
+    if (businessName && contactLegalName && country && streetAddress && city && stateProvince && zipPostalCode && email && phone) {
       router.push('/vendor/BusinessAdjective');
+      // This is where you would save the data to the database
+
     } else {
       alert("Please fill in all required fields.");
     }
@@ -56,21 +162,156 @@ const BusinessProfile = () => {
         <div className="w-[60%]">
           <h2 className="text-md text-gray-500">Step 01/05</h2>
           <h1 className="text-5xl font-bold mb-8">Create Business Profile</h1>
-          <div className="space-y-6">
-            <div className="mt-0">Business name</div>
+          <div className="text-xl">
+            <div className="">Business name<span className="text-red-500">*</span></div>
             <input
-              className="w-[70%] h-14 bg-gray-300 mt-2"
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
               value={businessName}
               onChange={handleBusinessNameChange}
               required
             />
-            <div className="">Legal Business name</div>
+
+            <div className="">Legal business name</div>
             <input
-              className="w-full h-14 bg-gray-300 "
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
               value={legalBusinessName}
               onChange={handleLegalBusinessNameChange}
               required
             />
+
+            <div className="">Contact legal name<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={contactLegalName}
+              onChange={handleContactLegalNameChange}
+              required
+            />
+
+            <div className="">Contact preferred name</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={contactPreferredName}
+              onChange={handleContactPreferredNameChange}
+              required
+            />
+
+            <div className="">Country/region<span className="text-red-500">*</span></div>
+            <select
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={country} 
+              onChange={(e) => setCountryName(e.target.value)}
+              required
+            >
+              <option value="">Select a country</option>
+              {countries.map((country) => (
+                <option key={country} value={country}>{country}</option>
+              ))}
+            </select>
+
+            <div className="">Street address<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={streetAddress}
+              onChange={handleStreetAddressChange}
+              required
+            />
+
+            <div className="">Apt, suite. (optional)</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={aptSuite}
+              onChange={handleAptSuiteChange}
+              required
+            />  
+
+            <div className="">City<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={city}
+              onChange={handleCityChange}
+              required
+            />
+
+            <div className="">State/province<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={stateProvince}
+              onChange={handleStateProvinceChange}
+              required
+            />
+
+            <div className="">Zip/postal code<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={zipPostalCode}
+              onChange={handleZipPostalCodeChange}
+              required
+            />
+
+            <div className="">Email<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />    
+
+            <div className="">Phone<span className="text-red-500">*</span></div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={phone}
+              onChange={handlePhoneChange}
+              required
+            />    
+
+            <div className="">Website</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={website}
+              onChange={handleWebsiteChange}
+              required
+            />    
+
+            <div className="">Number of employees</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={numberOfEmployees}
+              onChange={handleNumberOfEmployeesChange}
+              required
+            />    
+
+            <div className="">Description</div>
+            <input
+              className="w-[70%] h-40 bg-gray-300 mb-8 text-left align-top p-2"
+              value={description}
+              onChange={handleDescriptionChange}
+              required
+            />    
+
+            <div className="">Facebook link</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={facebookLink}
+              onChange={handleFacebookLinkChange}
+              required
+            />   
+
+            <div className="">Twitter "X" handle</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={twitterHandle}
+              onChange={handleTwitterHandleChange}
+              required
+            />   
+
+            <div className="">Instagram handle</div>
+            <input
+              className="w-[70%] h-14 bg-gray-300 mb-8 text-left align-top p-2"
+              value={instagramHandle}
+              onChange={handleInstagramHandleChange}
+              required
+            />   
+
           </div>
           <div className="flex space-x-6 mt-8">
             <div className="w-36 h-14 bg-gray-300 flex items-center justify-center">Help</div>
